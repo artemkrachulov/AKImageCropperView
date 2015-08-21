0bfc1d81b740775105f8fa716aab2ed0181efe75
Artem Krachulov <artem.krachulov@gmail.com>
2015-08-21 10:52:40 +0300

    Initial commit

---
+++ AKImageCropperDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AKImageCropperDemo
+//
+//  Created by Krachulov Artem  on 8/21/15.
+//  Copyright (c) 2015 Artem Krachulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
