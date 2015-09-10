34823b793cab3ac739d78ffd2f13c583246fb302
Artem Krachulov <artem.krachulov@gmail.com>
2015-09-10 11:27:33 +0300

    Update, v.0.8

---
+++ AKImageCropper/AKImageCropperScollView.swift
@@ -0,0 +1,21 @@
+//
+//  AKImageCropperScollView.swift
+//  AKImageCropperDemo
+//
+//  Created by Krachulov Artem  on 9/7/15.
+//  Copyright (c) 2015 Artem Krachulov. All rights reserved.
+//
+
+import UIKit
+
+class AKImageCropperScollView: UIScrollView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AKImageCropper/AKImageCropperTouchView.swift
@@ -0,0 +1,21 @@
+//
+//  AKImageCropperCropFrame.swift
+//  AKImageCropperDemo
+//
+//  Created by Krachulov Artem  on 9/2/15.
+//  Copyright (c) 2015 Artem Krachulov. All rights reserved.
+//
+
+import UIKit
+
+class AKImageCropperCropFrame: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
